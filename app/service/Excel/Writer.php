e9c59518ab1a9320ba35db3830697d93f6dde099
lemon <lemon@goldenname.com>
2020-06-19 14:18:56 +0800

    修改脚本

--- app/service/Excel/Writer.php
+++ app/service/Excel/Writer.php
@@ -11,36 +11,29 @@ class Writer{
 
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-        $sheet->getDefaultColumnDimension()->setWidth(15);
         $sheet->getDefaultRowDimension()->setRowHeight(15);
-        //写表头
-        if (!empty($results)){
-            $fields = array_keys($results[0]);
-        }else{
-            $fields = [];
-        }
-
-        $col = 65; //A
-        for ($i=0;$i<count($fields);$i++){
-            $sheet->setCellValue(chr($col)."1", $results[0][$fields[$i]]);
-            $sheet->getStyle(chr($col)."1")->getAlignment()->setWrapText(true)->setVertical(Alignment::VERTICAL_CENTER)->setHorizontal(Alignment::HORIZONTAL_LEFT);
-            $col++;
-        }
 
         //写数据
-        $row=2;
+        $row=1;
         foreach ($results as $k=>$data){
-            if($k==0){
-                continue;
-            }
             $col=65;
-            for ($i=0; $i<count($fields);$i++){
-                $sheet->setCellValue(chr($col).$row, $data[$fields[$i]]);
+            for ($colIndex=0; $colIndex<count($data);$colIndex++){
+                $colValue = $data[$colIndex];
+                if ($row == 1){
+                    if ($colIndex == count($data)-1){
+                        $sheet->getColumnDimension(chr($col))->setWidth(100);
+                    }else{
+                        $sheet->getColumnDimension(chr($col))->setWidth(strlen($colValue));
+                    }
+                }
+                $sheet->setCellValue(chr($col).$row, $colValue);
+                $sheet->getStyle(chr($col).$row)->getFont()->setSize(10);
                 $sheet->getStyle(chr($col).$row)->getAlignment()->setWrapText(true)->setVertical(Alignment::VERTICAL_CENTER)->setHorizontal(Alignment::HORIZONTAL_LEFT);
                 $col++;
             }
             $row++;
         }
+
         //直接指定路径保存
         $writer = new Xlsx($spreadsheet);
         $writer->save($path);
